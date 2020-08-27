aac8c83fdab2c6ecd28150eea8aa3edb85bce6c0
endret <pengogf@gmail.com>
2020-08-27 18:25:11 +0200

    first skeleton of the project

---
+++ src/ScaledDomains.Extensions.Caching.MySql/MySqlServerCache.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace ScaledDomains.Extensions.Caching.MySql
+{
+    public class MySqlServerCache : IDistributedCache
+    {
+        public byte[] Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> GetAsync(string key, CancellationToken token = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string key, byte[] value, DistributedCacheEntryOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options,
+            CancellationToken token = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Refresh(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RefreshAsync(string key, CancellationToken token = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(string key, CancellationToken token = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
