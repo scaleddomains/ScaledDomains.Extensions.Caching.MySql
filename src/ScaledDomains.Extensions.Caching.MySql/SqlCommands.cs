5ae08a54e558efb643d0e4c9ff942dee625e3397
Attila Ersek <attila.ersek@scaleddomains.com>
2020-08-28 14:35:32 +0200

    implement Set and Update operations

--- src/ScaledDomains.Extensions.Caching.MySql/SqlCommands.cs
+++ src/ScaledDomains.Extensions.Caching.MySql/SqlCommands.cs
@@ -6,11 +6,26 @@ namespace ScaledDomains.Extensions.Caching.MySql
         {
             var fullName = $"`{schemaName}`.`{tableName}`";
 
-            GetCache = string.Format(GetCacheFormat, fullName); // TODO update expiration
+            GetCache = string.Format(UpdateCacheFormat + GetCacheFormat, fullName);
+            SetCache = string.Format(SetCacheFormat, fullName);
         }
 
-        private const string GetCacheFormat = "SELECT Value FROM {0} WHERE Id = @Id AND ExpiresAt >= @ExpiresAt;";
+        private const string GetCacheFormat = "SELECT Value FROM {0} WHERE Id = @Id AND ExpiresAt >= @UtcNow;";
+
+        private const string UpdateCacheFormat =
+            "UPDATE {0} SET ExpiresAt = (CASE WHEN (SlidingExpiration IS NUll) THEN AbsoluteExpiration ELSE ADDTIME(@UtcNow, SlidingExpiration)), " +
+            "WHERE Id = @Id AND ExpiresAt <= @UtcNow AND SlidingExpiration IS NOT NULL AND (AbsoluteExpiration IS NULL OR AbsoluteExpiration >= ExpiresAt) ";
+            
+        private const string SetCacheFormat = 
+            "INSERT INTO {0} (Id, Value, ExpiresAt, SlidingExpiration, AbsoluteExpiration) VALUES (@Id, @Value, @ExpiresAt, @SlidingExpiration, @AbsoluteExpiration) " +
+            "ON DUPLICATE KEY UPDATE " +
+            "Value = @Value, " +
+            "ExpiresAt = (CASE WHEN (@SlidingExpiration IS NUll) THEN @AbsoluteExpiration ELSE ADDTIME(@UtcNow, @SlidingExpiration)), "+
+            "SlidingExpiration = @SlidingExpiration, "+
+            "AbsoluteExpiration = @AbsoluteExpiration;";
 
         internal readonly string GetCache;
+
+        internal readonly string SetCache;
     }
 }
